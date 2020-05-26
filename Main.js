d3eb3d737cb9766d314cffab26f60cbb6dfe0fb6
Animark159 <thornemark58@gmail.com>
2020-05-26 15:58:20 +0100

    New audio code.

--- Main.js
+++ Main.js
@@ -2,6 +2,8 @@ var SnapCount = 0;
 
 var S = document.getElementById("myAudio");
 
+console.log("Script loaded!");
+
 function Increment(id){
 	Image = document.getElementById("Button");
 	Image.src = "/static/ButtonB.png";
